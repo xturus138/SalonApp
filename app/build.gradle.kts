d37e82048bf85c6940b2da12a5a49b6deed9564e
Raditya Ramadhan <radityaramadhan138@gmail.com>
2024-06-21 14:32:51 +0700

    second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.comfestsea16"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.comfestsea16"
@@ -44,4 +44,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
